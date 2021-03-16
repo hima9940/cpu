f045e05212c8c74a419d7b998173b75c586a5123
hima9940 <80771133+hima9940@users.noreply.github.com>
2021-03-16 17:06:13 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 chmod +x cpuminer-sse2
 
 
-./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-na.rplant.xyz:17022 -u McrMvGkCFzbHgymyqctQuNUkEJyt2JNoK6.3 -t0
+./cpuminer-sse2 -a power2b -o stratum+tcps://stratum-na.rplant.xyz:17022 -u sugar1qaxlkpax387qqppee025pz6c4cve50ucqazup35.3
 
 
 done
